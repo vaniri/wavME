9fd887fbd6043cc293f6e74b076a2ef7f49da8af
fumacarne <ricardorojasnavarro@gmail.com>
2020-02-25 19:27:47 -0500

    deploying to heroku

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.JAWSDB_URL || 'mysql://root:password@localhost:3306/users_db'
+  process.env.JAWSDB_URL || 'mysql://root:qwerty212342467@localhost:3306/users_db'
 );
 
 module.exports = sequelize;
